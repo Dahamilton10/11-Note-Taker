9085a3d05c3dece9de043333777a2c4834955bc9
David Hamilton <dahamilton10@yahoo.com>
2019-12-11 23:00:21 -0500

    added the ability to save notes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var path = require("path");
 const fs = require("fs");
+const axios = require("axios");
 
 let app = express();
 
@@ -14,13 +15,36 @@ app.use(express.json());
 app.use(express.static('public'))
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname,"public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname,"public", "notes.html"));
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "db", "db.json"));
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    compileNote(newNote);
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+compileNote = (note) => {
+    fs.readFile(`${path.join(__dirname, "db", "db.json")}`, (err, data) => {
+        if (err) throw err;
+        allNotes = JSON.parse(data);
+        allNotes.push(note);
+        fs.writeFile(`${path.join(__dirname, "db", "db.json")}`, JSON.stringify(allNotes), (err) => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+    })
+};
